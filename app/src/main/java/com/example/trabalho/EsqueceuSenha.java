e791b24c186c567f77f092ccf1ac7be96f600a72
Silvio2 <atlasgamer457@gmail.com>
2024-06-06 14:48:08 -0300

    Asc

--- app/src/main/java/com/example/trabalho/EsqueceuSenha.java
+++ app/src/main/java/com/example/trabalho/EsqueceuSenha.java
@@ -1,28 +1,53 @@
 package com.example.trabalho;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class EsqueceuSenha extends AppCompatActivity {
     EditText emailRecuperacao;
     FirebaseAuth trocarSenha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_esqueceu_senha);
         iniciar();
-        trocarSenha =  ConfiguraBD.Fireautenticacao();
-    }
-    public void iniciar(){
-        emailRecuperacao = findViewById(R.id.EmailRecuperacao);
-    }
-    public void TrocarSenha(View v){
-        
+        trocarSenha = ConfiguraBD.Fireautenticacao();
     }
 
+    public void iniciar() {
+        emailRecuperacao = findViewById(R.id.EmailRecuperacao);
+    }
+
+    public void TrocarSenha(View v) {
+        String email = emailRecuperacao.getText().toString();
+
+        if (!TextUtils.isEmpty(email)) {
+            trocarSenha.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Toast.makeText(EsqueceuSenha.this, "Reset password link has been sent to the registered e-mail", Toast.LENGTH_SHORT).show();
+                    Intent in = new Intent(EsqueceuSenha.this, Login.class);
+                    startActivity(in);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(EsqueceuSenha.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+    }
 }
